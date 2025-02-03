1bddafae6633c2e829e85fd5cc35065e8d14aee7
Gopu Darsan <68003773+gops601@users.noreply.github.com>
2025-02-03 19:29:01 +0530

    My first commit for project

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 
 app=Flask(__name__)
+Jobs=[{
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bangalore',
+    'salary':'Rs 100000'
+       },
+       {
+    'id':2,
+    'title':'QA Engineer',
+    'location':'Pune',
+    'salary':'Rs 200000'
+       },
+       {
+    'id':1,
+    'title':'Full Stack Developer',
+    'location':'Trivandrum',
+   
+       }
+       ]
 
 @app.route("/")
 def hello():
-    return "<h2><b>Hello World<b></h2>"
+    return render_template('index.html',job=Jobs,companyname="Gops")
 
-
+@app.route("/jobs")
+def joblist():
+    return jsonify(Jobs)
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
